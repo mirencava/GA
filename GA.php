2a17b8c775c546c4de07f0495d86344c8806297e
Miren <miren.cava@gmail.com>
2019-03-17 18:04:02 +0100

    firts commit

---
+++ GA.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: ADD GA TAG
+*/
+
+// Hook the 'wp_head' action hook, add the function named 'setGA' to it
+add_action("wp_head", "setGA");
+
+
+function setGA()
+{
+  echo '<!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136408159-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-136408159-1');
+  </script>'; 
+
+}
